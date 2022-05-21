7b0ac032e2a29c1854da4136f416e5fcd3a8ebf4
jb-network <85036487+jb-network@users.noreply.github.com>
2022-05-21 00:21:33 -0500

    Add project files.

---
+++ Repairing_the_Clocktower/Program.cs
@@ -0,0 +1,4 @@
+﻿// This is challenge work for the "C# Players Guide"
+// Level 9 Repairing the Clocktower
+//The challenge accepts input from the console
+//It will display "Tick" if the number is even and "Tock" if the number is odd.
