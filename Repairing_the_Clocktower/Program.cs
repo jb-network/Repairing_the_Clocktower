e418e51e6cfb2e98ed6c8776df8f4f342f2777fd
jb-network <85036487+jb-network@users.noreply.github.com>
2022-05-21 00:33:45 -0500

    Finished Exercise

--- Repairing_the_Clocktower/Program.cs
+++ Repairing_the_Clocktower/Program.cs
@@ -2,3 +2,19 @@
 // Level 9 Repairing the Clocktower
 //The challenge accepts input from the console
 //It will display "Tick" if the number is even and "Tock" if the number is odd.
+
+Console.WriteLine("Enter a number to repair the clock: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+
+if (userNumber%2 == 0)
+{
+    Console.WriteLine("The number that you provide was even: Tick");
+}
+else
+{
+    Console.WriteLine("The number that you provide was odd: Tock");
+}
+
+Console.WriteLine("Press any key to end");
+Console.ReadKey();
