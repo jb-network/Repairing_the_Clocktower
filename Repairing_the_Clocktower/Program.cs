203b07096431be528a5c601b62612516b1f10cd8
jb-network <85036487+jb-network@users.noreply.github.com>
2022-05-21 00:41:12 -0500

    updated to add zero condtion

--- Repairing_the_Clocktower/Program.cs
+++ Repairing_the_Clocktower/Program.cs
@@ -3,18 +3,24 @@
 //The challenge accepts input from the console
 //It will display "Tick" if the number is even and "Tock" if the number is odd.
 
+//Capture number from user
 Console.WriteLine("Enter a number to repair the clock: ");
 int userNumber = Convert.ToInt32(Console.ReadLine());
 
-
-if (userNumber%2 == 0)
+//Simple if/else: 1) check for zero 2) check for even 3) else its an odd number
+if (userNumber == 0)
+{
+    Console.WriteLine("The number provided was zero. This is not even or odd");
+}          
+else if (userNumber%2 == 0)
 {
     Console.WriteLine("The number that you provide was even: Tick");
 }
-else
+else 
 {
     Console.WriteLine("The number that you provide was odd: Tock");
 }
+
 
 Console.WriteLine("Press any key to end");
 Console.ReadKey();
